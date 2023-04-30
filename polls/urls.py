dd55121b46020969529d65382de61263b391cac7
usersina <benna.anis2@gmail.com>
2023-04-30 22:14:51 +0100

    handle logic for votes

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),  # /polls/
     path('<int:question_id>/', views.detail, name='detail'),  # /polls/5/
     path('<int:question_id>/results/', views.results, name='results'),  # /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # /polls/5/vote/
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 # Fetch all questions and display them
@@ -30,3 +31,19 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {
         'question': question
     })
+
+
+# Vote for a question choice
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choices.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
